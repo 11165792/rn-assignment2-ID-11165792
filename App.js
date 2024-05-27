74ba532555ab04c6ce4b7ce6cfbdd1ae83148aeb
11165792 <sjdei@st.ug.edu.gh>
2024-05-27 17:42:47 +0100

    Changed font size

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Joy Dei</Text>
+      <Text style={styles.mainText}>My name is Joy Dei</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mainText:{
+    fontSize: 24,
+  }
 });
